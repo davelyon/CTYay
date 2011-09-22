9197c75bd66222aa30f7cad8cfc3df6007a624a6
Dave Lyon <dave@davelyon.net>
2011-09-22 15:38:19 -0500

    Whitespace

--- spec/ctyay/time_spec.rb
+++ spec/ctyay/time_spec.rb
@@ -7,6 +7,7 @@ describe Ctyay::CTATime do
       Ctyay::Config.bus_api_key=123
       FakeWeb.register_uri(:get, "http://www.ctabustracker.com/bustime/api/v1/gettime?key=123", :body => time_xml)
     end
+
     it "returns the official CTA time" do
       Ctyay::CTATime.time.should == Time.new("20111212 00:00:00")
     end
