e2a6b18c2541ad809eb7991e3878883f87b7e76f
Dave Lyon <dave@davelyon.net>
2011-09-22 14:33:07 -0500

    Add Stop.all to get all stops for route/direction

--- lib/ctyay/stop.rb
+++ lib/ctyay/stop.rb
@@ -2,12 +2,24 @@ module Ctyay
   class Stop
     attr_reader :id, :name, :latitude, :longitude
 
+    def self.all(route_id, direction)
+      parse Nokogiri::XML.parse(Request.new('/getstops', "&rt=#{route_id}&dir=#{URI.escape(direction)}").get)
+    end
+
     def initialize(id, name, latitude, longitude)
       @id, @name, @latitude, @longitude = id, name, latitude, longitude
     end
 
     def location
       [latitude, longitude]
+    end
+
+    private
+    def self.parse(xml)
+      xml.xpath("//stop").map do |stop|
+        new( stop.xpath("stpid").inner_text, stop.xpath("stpnm").inner_text,
+            stop.xpath("lat").inner_text, stop.xpath("lon").inner_text )
+      end
     end
   end
 end
--- spec/ctyay/stop_spec.rb
+++ spec/ctyay/stop_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe Ctyay::Stop do
+  let!(:stop_xml) { File.open("spec/data/stops.example.xml") }
+
+  before(:all) do
+    Ctyay::Config.bus_api_key = 123
+    FakeWeb.register_uri(:get, "http://www.ctabustracker.com/bustime/api/v1/getstops?key=123&rt=20&dir=East%20Bound", :body => stop_xml)
+  end
+
   subject { Ctyay::Stop.new("1", "Lake/Halsted", "42.88842", "-37.66532") }
 
   describe "#id" do
@@ -23,5 +30,20 @@ describe Ctyay::Stop do
     it { subject.location.should == ["42.88842", "-37.66532"] }
   end
 
+  context "remote api access" do
+
+    describe ".all" do
+      let(:stop_1) { double("Stop") }
+      let(:stop_2) { double("Stop") }
+      before do
+        Ctyay::Stop.should_receive(:new).with("4727", "1633 W Madison", "41.881408012088", "-87.668516635895").and_return( stop_1 )
+        Ctyay::Stop.should_receive(:new).with("449", "Washington & Wabash", "41.883229232153", "-87.625987529755").and_return( stop_2 )
+      end
+
+      it "returns a collection of Stops" do
+        Ctyay::Stop.all("20", "East Bound").should == [stop_1, stop_2]
+      end
+    end
+  end
 
 end
